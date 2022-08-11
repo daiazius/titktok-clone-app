47aace944f9e121f3aa575c1e0b10ddb059b9e35
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2022-08-11 13:07:08 -0300

    few changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'i.kym-cdn.com',
+      'lh3.googleusercontent.com'
+    ],
+  }
 }
 
 module.exports = nextConfig
